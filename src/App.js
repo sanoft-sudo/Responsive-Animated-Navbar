d44fc1738cff689c7f98a2c71405732814e78aae
Sanjar Juraev <72341995+sanoft-sudo@users.noreply.github.com>
2021-07-12 22:59:05 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import ResponsiveNavbar3 from "./components/ResponsiveNavbar3";
+
+export default function App() {
+  return (
+    <div className="App">
+      <ResponsiveNavbar3 />
+    </div>
+  );
+}
